243b21dace06ab076cceb5fce4357fa11b47a3ea
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2021-11-03 11:18:32 +0900

    サーバー立ち上げ修正

--- go/src/work/main.go
+++ go/src/work/main.go
@@ -25,18 +25,24 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: "test", Body: body}, nil
+	return &Page{Title: title, Body: body}, nil
 }
 
 /* ------ viewHandler ------ */
+/* http.ResponseWriterと*http.Requestを記述する必要がある（HandleFuncの定義元にジャンプすると引数として渡してあるため）*/
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	/* /view/test */
+	/* http.Requestにリクエストを出した中身のデータが入っているため、r.URL.PathでURLが取得できる */
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
+
 func main() {
 	/*----- もしviewというURLが来た場合viewHandlerにいく */
+	/* 自分のハンドラーを立ち上げたい場合は、ListenAndServeの前にhttp.HandleFuncのメソッドを使用し、ハンドラーを登録する必要がある */
 	http.HandleFunc("/view/", viewHandler)
 	/*----- webサーバーの立ち上げ ----- */
+	/* ハンドルがnilの為、/view/以外にアクセスしようとするとnot foundが返る */
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
