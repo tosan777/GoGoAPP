f96f9bed49974c3d253a4259f75f98aa65cacc85
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2021-11-03 11:45:50 +0900

    テンプレート作成

--- go/src/work/main.go
+++ go/src/work/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 type Page struct {
@@ -28,6 +28,11 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 /* ------ viewHandler ------ */
 /* http.ResponseWriterと*http.Requestを記述する必要がある（HandleFuncの定義元にジャンプすると引数として渡してあるため）*/
 func viewHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,13 +40,26 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	/* http.Requestにリクエストを出した中身のデータが入っているため、r.URL.PathでURLが取得できる */
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+/* ------ editHandler ------ */
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	/* /view/test */
+	/* http.Requestにリクエストを出した中身のデータが入っているため、r.URL.PathでURLが取得できる */
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
 	/*----- もしviewというURLが来た場合viewHandlerにいく */
 	/* 自分のハンドラーを立ち上げたい場合は、ListenAndServeの前にhttp.HandleFuncのメソッドを使用し、ハンドラーを登録する必要がある */
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	/*----- webサーバーの立ち上げ ----- */
 	/* ハンドルがnilの為、/view/以外にアクセスしようとするとnot foundが返る */
 	log.Fatal(http.ListenAndServe(":8080", nil))
